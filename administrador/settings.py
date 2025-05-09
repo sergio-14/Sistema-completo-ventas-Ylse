98f7424763d9ade79338b2733367ac399ff76997
sergio <elmatadorsergio1234@gmail.com>
2025-05-09 10:42:39 -0400

    segundo cambio

--- administrador/settings.py
+++ administrador/settings.py
@@ -142,3 +142,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+ALLOWED_HOSTS = ['localhost','web-production-3dfda.up.railway.app']
